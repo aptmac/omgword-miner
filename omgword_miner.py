0e56c5d29504c494cd3a3a6941fab66f1d4d63cb
Alex Macdonald <almacdon@redhat.com>
2020-01-09 14:54:00 -0500

    initial dump of the omgword mining bot

---
+++ omgword_miner.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+
+# おまえはもうしんでいる。
+# QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+# QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+# QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+# QQQQQQQQQQQQQQQQQQQWQQQQQWWWBBBHHHHHHHHHBWWWQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+# QQQQQQQQQQQQQQQD!`__ssaaaaaaaaaass_ass_s____.  -~""??9VWQQQQQQQQQQQQQQQQQQQ
+# QQQQQQQQQQQQQP'_wmQQQWWBWV?GwwwmmWQmwwwwwgmZUVVHAqwaaaac,"?9$QQQQQQQQQQQQQQ
+# QQQQQQQQQQQW! aQWQQQQW?qw#TTSgwawwggywawwpY?T?TYTYTXmwwgZ$ma/-?4QQQQQQQQQQQ
+# QQQQQQQQQQW' jQQQQWTqwDYauT9mmwwawww?WWWWQQQQQ@TT?TVTT9HQQQQQQw,-4QQQQQQQQQ
+# QQQQQQQQQQ[ jQQQQQyWVw2$wWWQQQWWQWWWW7WQQQQQQQQPWWQQQWQQw7WQQQWWc)WWQQQQQQQ
+# QQQQQQQQQf jQQQQQWWmWmmQWU???????9WWQmWQQQQQQQWjWQQQQQQQWQmQQQQWL 4QQQQQQQQ
+# QQQQQQQP'.yQQQQQQQQQQQP"       <wa,.!4WQQQQQQQWdWP??!"??4WWQQQWQQc ?QWQQQQQ
+# QQQQQP'_a.<aamQQQW!<yF "!` ..  "??$Qa "WQQQWTVP'    "??' =QQmWWV?46/ ?QQQQQ
+# QQQP'sdyWQP?!`.-"?46mQQQQQQT!mQQgaa. <wWQQWQaa _aawmWWQQQQQQQQQWP4a7g -WWQQ
+# QQ[ j@mQP'adQQP4ga, -????" <jQQQQQWQQQQQQQQQWW;)WQWWWW9QQP?"`  -?QzQ7L ]QQQ
+# QW jQkQ@ jWQQD'-?$QQQQQQQQQQQQQQQQQWWQWQQQWQQQc "4QQQQa   .QP4QQQQfWkl jQQQ
+# QE ]QkQk $D?`  waa "?9WWQQQP??T?47`_aamQQQQQQWWQw,-?QWWQQQQQ`"QQQD\Qf(.QWQQ
+# QQ,-Qm4Q/-QmQ6 "WWQma/  "??QQQQQQL 4W"- -?$QQQQWP`s,awT$QQQ@  "QW@?$:.yQQQQ
+# QQm/-4wTQgQWQQ,  ?4WWk 4waac -???$waQQQQQQQQF??'<mWWWWWQW?^  ` ]6QQ' yQQQQQ
+# QQQQw,-?QmWQQQQw  a,    ?QWWQQQw _.  "????9VWaamQWV???"  a j/  ]QQf jQQQQQQ
+# QQQQQQw,"4QQQQQQm,-$Qa     ???4F jQQQQQwc <aaas _aaaaa 4QW ]E  )WQ`=QQQQQQQ
+# QQQQQQWQ/ $QQQQQQQa ?H ]Wwa,     ???9WWWh dQWWW,=QWWU?  ?!     )WQ ]QQQQQQQ
+# QQQQQQQQQc-QWQQQQQW6,  QWQWQQQk <c                             jWQ ]QQQQQQQ
+# QQQQQQQQQQ,"$WQQWQQQQg,."?QQQQ'.mQQQmaa,.,                . .; QWQ.]QQQQQQQ
+# QQQQQQQQQWQa ?$WQQWQQQQQa,."?( mQQQQQQW[:QQQQm[ ammF jy! j( } jQQQ(:QQQQQQQ
+# QQQQQQQQQQWWma "9gw?9gdB?QQwa, -??T$WQQ;:QQQWQ ]WWD _Qf +?! _jQQQWf QQQQQQQ
+# QQQQQQQQQQQQQQQws "Tqau?9maZ?WQmaas,,    --~-- ---  . _ssawmQQQQQQk 3QQQQWQ
+# QQQQQQQQQQQQQQQQWQga,-?9mwad?1wdT9WQQQQQWVVTTYY?YTVWQQQQWWD5mQQPQQQ ]QQQQQQ
+# QQQQQQQWQQQQQQQQQQQWQQwa,-??$QwadV}<wBHHVHWWBHHUWWBVTTTV5awBQQD6QQQ ]QQQQQQ
+# QQQQQQQQQQQQQQQQQQQQQQWWQQga,-"9$WQQmmwwmBUUHTTVWBWQQQQWVT?96aQWQQQ ]QQQQQQ
+# QQQQQQQQQQWQQQQWQQQQQQQQQQQWQQma,-?9$QQWWQQQQQQQWmQmmmmmQWQQQQWQQW(.yQQQQQW
+# QQQQQQQQQQQQQWQQQQQQWQQQQQQQQQQQQQga%,.  -??9$QQQQQQQQQQQWQQWQQV? sWQQQQQQQ
+# QQQQQQQQQWQQQQQQQQQQQQQQWQQQQQQQQQQQWQQQQmywaa,;~^"!???????!^`_saQWWQQQQQQQ
+# QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQWWWWQQQQQmwywwwwwwmQQWQQQQQQQQQQQ
+# QQQQQQQWQQQWQQQQQQWQQQWQQQQQWQQQQQQQQQQQQQQQQWQQQQQWQQQWWWQQQQQQQQQQQQQQQWQ
+
+# Modified from: https://linuxacademy.com/blog/geek/creating-an-irc-bot-with-python3/
+# .. but this script runs with Python 2
+
+# Import some necessary libraries.
+import socket, re, subprocess, os, time, threading, sys, math, datetime
+from datetime import date
+
+# Some basic variables used to configure the bot        
+server = "SERVER" # Server
+channel = "CHANNEL" # Channel
+botnick = "BOT NICK" # Your bots nick
+password = "password"
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
+ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "\n") # user authentication
+ircsock.send("NICK "+ botnick +"\n") # assign the nick to the bot
+ircsock.send("nickserv identify " + password + "\r\n")
+
+def ping(): # respond to server Pings.
+  ircsock.send("PONG :pingis\n")  
+
+def sendmsg(msg): # sends messages to the channel.
+  ircsock.send("PRIVMSG "+ channel +" :"+ msg +"\n") 
+
+def joinchan(chan): # join channel(s).
+  ircsock.send("JOIN "+ chan + "\n")
+
+def whisper(msg, user): # whisper a user 
+  ircsock.send("PRIVMSG " + user + ' :' + msg.strip('\n\r') + '\n')
+
+def get_names(): # get list of users in a channel
+  ircsock.send("NAMES " + channel + "\n")
+  resp = ircsock.recv(2048)
+  return re.search(".*" + channel + " :(.*)", resp, re.IGNORECASE).group(1)
+
+def unscramble(word):
+  # code borrowed from:
+  # https://www.reddit.com/r/dailyprogrammer/comments/11shra/10202012_challenge_105_easy_word_unscrambler/c6pj0qu/
+  words = {''.join(sorted(x.strip().lower())):x.strip() for x in open('PATH TO DICTIONARY')}
+  findMatch = lambda y: words[''.join(sorted(y.lower()))]
+  return findMatch(word)
+
+# main functions of the bot
+def main():
+  # start by joining the channel.
+  joinchan(channel)
+  # start infinite loop to continually check for and receive new info from server
+  while 1: 
+    # clear ircmsg value every time
+    ircmsg = ""
+    # set ircmsg to new data received from server
+    ircmsg = ircsock.recv(2048)
+    # remove any line breaks
+    ircmsg = ircmsg.strip('\n\r') 
+    # print received message to stdout (mostly for debugging).
+    print(ircmsg) 
+    # repsond to pings so server doesn't think we've disconnected
+    if ircmsg.find("PING :") != -1: 
+      ping()
+    # look for PRIVMSG lines as these are messages in the channel or sent to the bot
+    if ircmsg.find("PRIVMSG") != -1:
+      # save user name into name variable
+      name = ircmsg.split('!',1)[0][1:]
+      print('name: ' + name)
+      # get the message to look for commands
+      message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
+      print(message)
+      if name == 'OmgB0t1':
+        try:
+          word = re.search("^([a-z]+)$", message).group(1)
+          sendmsg("!answer " + unscramble(word))
+        except AttributeError as e:
+          print(e)
+          if message[:5] == "Sorry":
+            sendmsg(":(")
+          if message[:9] == "Game over" or message[:8] == "Congrats":
+            sendmsg("!omgword")
+      else:
+      # if no command found, get 
+        if len(name) < 17:
+          if name.lower() == "YOUR NICK" and message == "cheese it!":
+            sendmsg("O.O")
+            ircsock.send("PART " + channel + "\r\n")
+            sys.exit()
+
+#start main function
+main()
